62388804b2a181237158aae6d2f9f6bf19178bcf
Fulano de Tal <aras.strong8@gmail.com>
2021-09-24 19:38:07 -0300

    feat: componente svelte

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	import Header from "./componentes/header.svelte";
+	const props = {
+		url: "https://servicodados.ibge.gov.br/api/v1/localidades/estados",
+		title: "A list of todos",
+	};
+</script>
+
+<main>
+	<Header  {...props} let:uf>
+		<h1>lista de uf brasileiros</h1>
+		<ul>
+			{#each uf as nome}
+			  <li>{nome.nome}</li>
+			{/each}
+		  </ul>
+	</Header>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+      li{
+        list-style: none;
+      }
+
+</style>
---
+++ src/componentes/header.svelte
@@ -0,0 +1,14 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let url 
+  let uf = [];
+
+  onMount(async function() {
+    const response = await fetch(url);
+    const json = await response.json();
+    uf = json;
+  });
+</script>
+
+<slot {uf}/>
